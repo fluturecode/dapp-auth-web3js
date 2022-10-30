5756d5bfabaf36af8fed01d6f3c41ffb8f99ddcd
Fluturecode <fluturecode@gmail.com>
2022-10-30 13:54:34 -0400

    added onConnect function to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import Web3 from 'web3';
 import './App.css';
 
 function App() {
+
+  const [isConnected, setIsConnected] = useState(false);
+
   const detectCurrentProvider = () => {
     let provider;
     if (window.ethereum) {
@@ -14,10 +18,35 @@ function App() {
     return provider;
   }
 
+  const onConnect = async() => {
+    try {
+      const currentProvider = detectCurrentProvider();
+      if(currentProvider) {
+        await currentProvider.request({method: 'eth_requestAccounts'});
+        const web3 = new Web3(currentProvider);
+        const userAccount = await web3.eth.getAccounts();
+        const account = userAccount[0];
+        let ethBalance = await web3.eth.getBalance(account);
+        setIsConnected(true);
+        }
+    } catch(err) {
+      console.log(err);
+    }
+  }
+
   return (
     <div className='app'>
       <div className='app-header'>
         <h1>React DAPP with Auths</h1>
+      </div>
+      <div classsName='app-wrapper'>
+        {!isConnected && (
+          <div>
+            <button className='app-button_login' onClick={onConnect}>
+              Login
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
